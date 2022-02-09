285d913885a69c481c6ffdcf0d6cfdf362f441ce
John Edwards <edwards@Johns-MacBook-Pro.local>
2022-02-09 15:31:45 -0700

    Updated keystroke playback

--- KeystrokePlayback/main.js
+++ KeystrokePlayback/main.js
@@ -1,11 +1,14 @@
 var loadingWidget = document.getElementById('loading');
 loadingWidget.style.visibility = 'hidden';
-var dropdown = document.getElementById('files');
+
+var subjectsWidget = document.getElementById('subjects');
+var assignmentsWidget = document.getElementById('assignments');
+var filesWidget = document.getElementById('files');
+
 var table = document.getElementById('table');
 var slider = document.getElementById('slider');
 var editNumWidget = document.getElementById('edit-num');
 var eventNumWidget = document.getElementById('event-num');
-// var filepathWidget = document.getElementById('filepath');
 var textarea = document.getElementById('textarea');
 editNumWidget.innerHTML = slider.value; // Display the default slider value
 
@@ -15,71 +18,75 @@ function removeAllChildNodes(parent) {
     }
 }
 
-function pypChanged(event) {
-  // console.log('here');
-  const fileList = event.target.files;
-  if (fileList.length > 0) {
-    ET_EDIT = 'e';
-    const file = fileList[0];
-    let header = 'EventType,InsertText,DeleteText,CodeStateSection,ClientTimestamp,X-File,Version,X-Session\n'
-    readFile(file, header);
-  }
-}
+// function pypChanged(event) {
+//   // console.log('here');
+//   const fileList = event.target.files;
+//   if (fileList.length > 0) {
+//     ET_EDIT = 'e';
+//     const file = fileList[0];
+//     let header = 'EventType,InsertText,DeleteText,CodeStateSection,ClientTimestamp,X-File,Version,X-Session\n'
+//     readFile(file, header);
+//   }
+// }
 
-function sywChanged(event) {
-  pypChanged(event);
-}
+// function sywChanged(event) {
+//   pypChanged(event);
+// }
 
 function ps2Changed(event) {
   const fileList = event.target.files;
   if (fileList.length > 0) {
-    ET_EDIT = 'File.Edit';
     const file = fileList[0];
     readFile(file, '');
   }
 }
 
 function onload() {
-  // testSpreadsheet();
-  
-  document.getElementById('pyp-selector').addEventListener('change', pypChanged);
-  document.getElementById('syw-selector').addEventListener('change', sywChanged);
   document.getElementById('ps2-selector').addEventListener('change', ps2Changed);
-  // const fileSelector = document.getElementById('file-selector');
-  // fileSelector.addEventListener('change', (event) => {
-  //   const fileList = event.target.files;
-  //   if (fileList.length > 0) {
-  //     const file = fileList[0];
-  //     // filepathWidget.innerHTML = file;
-  //     let header = 'EventType,InsertText,DeleteText,CodeStateSection,ClientTimestamp,X-File,Version,X-Session,2,3\n'
-  //     readFile(file, header);
-  //   }
-  // });
 
   // Load a file automatically for testing
   $.ajax({
     async:true,
-    url: 'deident.csv',
-    // url: 'spencer.csv',
+    // url: 'deident.csv',
+    url: 'test.csv',
     dataType: 'text',
     success: function(data) 
     {
-      let header = 'EventType,InsertText,DeleteText,CodeStateSection,ClientTimestamp,X-File,Version,X-Session,2,3\n'
-      parseCSV(header+data);
+      parseCSV(data);
     }
   });
 }
 
+// All data
 let dfall = null;
+// Data for the selected subject
+let dfSubject = null;
+// Data for the selected subject/assignment
+let dfAssign = null;
+// Data for the selected subject/assignment/file
 let df = null;
 let editNum2rowNum = null;
 let file = null;
-let ET_EDIT = 'e';
 
 function parseCSV(data) {
   loadingWidget.style.visibility = 'visible';
-  // console.log('parsing');
+
+  let maxSize = 30000000;
+  if (data.length > maxSize) {
+    window.alert('Max size exceeded. Truncating data. '+
+                 'If data was not sorted by timestamp then unexpected behavior may occur.');
+    data = data.slice(0, maxSize);
+  }
+
   dfall = $.csv.toObjects(data);
+
+  // Sort the original file
+  dfall.sort((a,b) => {
+    return a.ClientTimestamp - b.ClientTimestamp;
+  });
+
+
+  let i = 0;
   dfall.forEach(row => {
     if (row['X-Session']) {
       row['X-Session'] = +row['X-Session'];
@@ -87,60 +94,99 @@ function parseCSV(data) {
       row['X-Session'] == null;
     }
     row.ClientTimestamp = +row.ClientTimestamp;
+    row['EventIdx'] = i;
+    i++;
   });
 
-  // Sort the original file
-  dfall.sort((a,b) => {
-    // if (a.Session == null || a.Session == b.Session) {
-      return a.ClientTimestamp - b.ClientTimestamp;
-    // }
-    // return 0;//a.Session - b.Session;
-  });
+  updateSubjectWidget();
+  loadingWidget.style.visibility = 'hidden';
+}
 
-  // Get files
-  let files = new Set();
+function updateSubjectWidget() {
+  let subjects = new Set();
   dfall.forEach(row => {
+    subjects.add(row['SubjectID']);
+  });
+
+  removeAllChildNodes(subjectsWidget);
+  subjects.forEach(file => {
+    var element = document.createElement("option");
+    element.innerText = file;
+    subjectsWidget.append(element);
+  });
+
+  subjectChanged();
+}
+
+function subjectChanged() {
+  subject = subjectsWidget.value;
+  dfSubject = dfall.filter(row => row.EventType == 'File.Edit' && row['SubjectID'] == subject);
+  updateAssignmentWidget();
+}
+
+function updateAssignmentWidget() {
+  let assignments = new Set();
+  dfSubject.forEach(row => {
+    assignments.add(row['AssignmentID']);
+  });
+
+  removeAllChildNodes(assignmentsWidget);
+  assignments.forEach(file => {
+    var element = document.createElement("option");
+    element.innerText = file;
+    assignmentsWidget.append(element);
+  });
+
+  assignmentChanged();
+}
+
+function assignmentChanged() {
+  assignment = assignmentsWidget.value;
+  dfAssign = dfSubject.filter(row => row['AssignmentID'] == assignment);
+  updateFileWidget();
+}
+
+function updateFileWidget() {
+  let files = new Set();
+  dfAssign.forEach(row => {
     files.add(row['X-File']);
   });
-  removeAllChildNodes(dropdown);
+
+  removeAllChildNodes(filesWidget);
   files.forEach(file => {
     var element = document.createElement("option");
     element.innerText = file;
-    dropdown.append(element);
+    filesWidget.append(element);
   });
 
-  file = dropdown.value;
+  file = filesWidget.value;
   fileChanged();
-  loadingWidget.style.visibility = 'hidden';
 }
 
 function fileChanged() {
   loadingWidget.style.visibility = 'visible';
-  file = dropdown.value;
-  updateEditNum2RowNum(dfall, file);
+  file = filesWidget.value;
 
-  df = dfall.filter(row => row.EventType == ET_EDIT && row['X-File'] == file);
+  df = dfAssign.filter(row => row['X-File'] == file);
   slider.max = df.length-1;
   slider.value = slider.max;
+  // slider.value = 0;
   editNumWidget.innerHTML = slider.value;
   reconstruct(df);
   loadingWidget.style.visibility = 'hidden';
 }
 
-function updateEditNum2RowNum(dfall, file) {
-  editNum2rowNum = new Array(dfall.length);
-  let nextEditNum = 0;
-  for (let i = 0; i < dfall.length; ++i) {
-    let row = dfall[i];
-    if (row.EventType && row.EventType == ET_EDIT && row['X-File'] == file) {
-      editNum2rowNum[nextEditNum] = i;
-      nextEditNum++;
-    }
-  }
-}
-
 function reconstruct(df) {
+  table.innerHTML = '';
+  textarea.value = '';
+
+  // console.log(df);
+  if (df.length == 0) {
+    return;
+  }
+
   let s = '';
+  // Reconstruct the file
   if (df.length > 0) {
     for (let i = 0; i <= slider.value; ++i) {
       let row = df[i];
@@ -153,9 +199,10 @@ function reconstruct(df) {
       }
     }
   }
-  // console.log('Reconstructed', s, df.length, slider.value);
+
   textarea.value = s;
-  eventNum = editNum2rowNum[slider.value];
+  
+  eventNum = df[slider.value].EventIdx;
   eventNumWidget.innerHTML = eventNum;
 
   // Update the table
@@ -182,8 +229,7 @@ function reconstruct(df) {
     let del = row.DeleteText.length<20 ? row.DeleteText :
         `<b>${row.DeleteText.slice(0,m)}...${row.DeleteText.slice(-m)}[${row.DeleteText.length}]</b>`;
     del = del.replace(' ', '&bull;');
-    // s += `<tr ${selected}> <td>${i}</td> <td>${row.EventType}</td> <td>${row.InsertText}</td> <td>${row.DeleteText}</td> <td>${row.CodeStateSection}</td> <td>${row.ClientTimestamp}</td> <td>${row.Session}</td>\n`;
-    s += `<tr ${selected}> <td>${row['X-File']}</td> <td>${i}</td> <td>${row.EventType}</td> <td>${insert}</td> <td>${del}</td> <td>${row.CodeStateSection}</td> <td>${row.ClientTimestamp}</td> <td>${row.Session}</td>\n`;
+    s += `<tr ${selected}> <td>${row['X-File']}</td> <td>${row.EventIdx}</td> <td>${row.EventType}</td> <td>${insert}</td> <td>${del}</td> <td>${row.CodeStateSection}</td> <td>${row.ClientTimestamp}</td> <td>${row.Session}</td>\n`;
   }
 
   table.innerHTML = s;
